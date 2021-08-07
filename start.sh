23dbcb39874bbac81236791a2573eb5fad1d6793
Christopher Käding <19961227+Cuube@users.noreply.github.com>
2021-08-07 23:39:48 +0200

    Initial Commit

---
+++ start.sh
@@ -0,0 +1,12 @@
+
+function launch() {
+    java -Xms2G -Xmx2G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar "purpur-1.17.jar" nogui
+    read -p "ee"
+}
+
+if [ ! -f "./purpur-1.17.jar" ]; then
+    echo "Downloading libraries..."
+    curl -o "purpur-1.17.jar" "https://api.pl3x.net/v2/purpur/1.17/latest/download"
+fi
+
+launch
